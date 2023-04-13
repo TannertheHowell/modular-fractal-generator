0844a2cce5d693da3dfe40ae7e3f7da453b3a0e8
Tanner Howell <a02079061@usu.edu>
2023-04-13 12:15:52 -0600

    Added a 4th concrete subclass for a Mandelbrot^3 class

---
+++ src/Mandelbrot3.py
@@ -0,0 +1,17 @@
+from Fractal import Fractal
+
+
+class Mandelbrot3(Fractal):
+    def __init__(self, max_iterations=100):
+        super().__init__(max_iterations)
+
+    def count(self, complex_number):
+        """
+        Return iteration count of the Mandelbrot^3 set for a point at the complex_number
+        """
+        z = complex(0, 0)  # z0
+        for i in range(self.max_iterations):
+            z = z * z * z + complex_number  # Get z1, z2, ...
+            if abs(z) > 2.0:
+                return i
+        return self.max_iterations - 1
