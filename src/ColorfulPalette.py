d11de3ff890cc869cbcc4eca00913185e61c515a
Tanner Howell <a02079061@usu.edu>
2023-04-14 18:32:55 -0600

    Updated the fractal's color palettes to be more vibrant

--- src/ColorfulPalette.py
+++ src/ColorfulPalette.py
@@ -6,8 +6,8 @@ class ColorfulPalette(Palette):
     def __init__(self, size):
         super().__init__(size)
 
-        split_size = size // 8 + 1
-        extras = size % 8
+        split_size = size // 16 + 1
+        extras = size % 16
 
         red = Color('red')
         yel = Color('yellow')
@@ -17,28 +17,24 @@ class ColorfulPalette(Palette):
         mag = Color('magenta')
         blk = Color('black')
 
-        self.palette = [c.hex_l for c in blu.range_to(yel, split_size)]
+        self.palette = [c.hex_l for c in blk.range_to(yel, split_size)]
         self.palette += [c.hex_l for c in yel.range_to(mag, split_size)][1:]
-        self.palette += [c.hex_l for c in mag.range_to(grn, split_size)][1:]
+        self.palette += [c.hex_l for c in blk.range_to(grn, split_size)][1:]
         self.palette += [c.hex_l for c in grn.range_to(blk, split_size)][1:]
         self.palette += [c.hex_l for c in blk.range_to(yel, split_size)][1:]
-        self.palette += [c.hex_l for c in yel.range_to(red, split_size)][1:]
-        self.palette += [c.hex_l for c in red.range_to(cya, split_size)][1:]
+        self.palette += [c.hex_l for c in blu.range_to(red, split_size)][1:]
+        self.palette += [c.hex_l for c in blk.range_to(cya, split_size)][1:]
+        self.palette += [c.hex_l for c in cya.range_to(blk, split_size)][1:]
+        self.palette += [c.hex_l for c in blk.range_to(yel, split_size)][1:]
+        self.palette += [c.hex_l for c in yel.range_to(mag, split_size)][1:]
+        self.palette += [c.hex_l for c in blk.range_to(grn, split_size)][1:]
+        self.palette += [c.hex_l for c in grn.range_to(red, split_size)][1:]
+        self.palette += [c.hex_l for c in red.range_to(yel, split_size)][1:]
+        self.palette += [c.hex_l for c in blu.range_to(red, split_size)][1:]
+        self.palette += [c.hex_l for c in blk.range_to(cya, split_size)][1:]
         self.palette += [c.hex_l for c in cya.range_to(grn, split_size)][1:]
         if extras != 0:
             self.palette += [c.hex_l for c in grn.range_to(yel, extras)][1:]
 
     def getColor(self, n):
         return self.palette[n]
-
-
-        # self.palette = [c.hex_l for c in blu.range_to(yel, 8)]
-        # self.palette += [c.hex_l for c in yel.range_to(mag, split_size)][1:]
-        # self.palette += [c.hex_l for c in mag.range_to(grn, split_size)][1:]
-        # self.palette += [c.hex_l for c in grn.range_to(blk, split_size)][1:]
-        # self.palette += [c.hex_l for c in blk.range_to(yel, split_size)][1:]
-        # self.palette += [c.hex_l for c in yel.range_to(red, split_size)][1:]
-        # self.palette += [c.hex_l for c in red.range_to(cya, split_size)][1:]
-        # self.palette += [c.hex_l for c in cya.range_to(grn, split_size)][1:]
-        # if extras != 0:
-        #     self.palette += [c.hex_l for c in grn.range_to(yel, extras+100)][1:]
--- src/DarkPalette.py
+++ src/DarkPalette.py
@@ -5,8 +5,10 @@ from colour import Color
 class DarkPalette(Palette):
     def __init__(self, size):
         super().__init__(size)
-        start = Color('#000000')
-        self.palette = [c.hex_l for c in start.range_to('#FFFFFF', size)]
+        black = Color('#000000')
+        white = Color('#FFFFFF')
+        self.palette = [c.hex_l for c in black.range_to(white, size // 2)]
+        self.palette += [c.hex_l for c in white.range_to(black, size // 2 + 1)][1:]
 
     def getColor(self, n):
         return self.palette[n]
